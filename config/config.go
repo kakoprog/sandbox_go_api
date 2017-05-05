f7416485ced6a2ded3c2af9cc5b28797fcee079e
kakoprog <kakoprog@gmail.com>
2017-05-06 07:24:09 +0900

    設定を外部ファイルから取れるように

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+  "io/ioutil"
+  "gopkg.in/yaml.v2"
+)
+
+type Dynamic map[interface{}]interface{}
+
+func Read(keys ...interface{}) interface{} {
+  buf, err := ioutil.ReadFile("./config.yaml")
+  if err != nil {
+    panic(err)
+  }
+
+  m := make(Dynamic)
+  err = yaml.Unmarshal(buf, &m)
+  if err != nil {
+    panic(err)
+  }
+
+  confval := m[keys[0]]
+  for _, key := range keys[1:] {
+    confval = confval.(Dynamic)[key]
+  }
+
+  return confval
+}
