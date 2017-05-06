b462c72a202028b03a7ec679a6c63f091531e9a1
kakoprog <kakoprog@gmail.com>
2017-05-06 15:52:41 +0900

    port番号をconfigから取るように

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo"
+	"github.com/kakoprog/sandbox_go_api/config"
+	"strconv"
 )
 
 func main() {
+	port := config.Read("server", "port").(int)
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":2300"))
+	e.Logger.Fatal(e.Start(":" + strconv.Itoa(port)))
 }
